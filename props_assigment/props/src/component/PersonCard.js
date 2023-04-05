19dcb0aeb535bbf150ca2e29e3f2a53e1e4064d7
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2023-04-05 16:37:17 -0700

    Add files via upload

---
+++ props_assigment/props/src/component/PersonCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PersonCard = (props) => {
+  const { firstName, lastName, age, hairColor } = props;
+  return (
+    <div>
+      <h1>
+        {firstName}, {lastName}
+      </h1>
+      <p>Age: {age}</p>
+      <p>Hair Color: {hairColor}</p>
+    </div>
+  );
+};
+
+export default PersonCard;

1defaadb52c319ac551766cd39d9161e45b21dbf
Yusuf Karakaya <yusufkarakaya92@gmail.com>
2023-04-06 10:56:07 -0700

    added validations

--- hook_form/src/component/Form.js
+++ hook_form/src/component/Form.js
@@ -2,63 +2,98 @@ import React, { useState } from 'react';
 
 export default function Form() {
   const [username, setUsername] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+
   const [lastname, setLastname] = useState('');
+  const [lastnameError, setLastnameError] = useState('');
+
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
   const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
   const [confirmpassword, setConfirmpassword] = useState('');
+  const [confirmpasswordError, setConfirmpasswordError] = useState('');
+
+  const handleFirstname = (event) => {
+    setUsername(event.target.value);
+    if (event.target.value.length < 2) {
+      setUsernameError('Title must be at least 2 character.');
+    } else {
+      setUsernameError('');
+    }
+  };
+
+  const handleLastname = (event) => {
+    setLastname(event.target.value);
+    if (event.target.value.length < 2) {
+      setLastnameError('Last name must bee at least 2 character.');
+    } else {
+      setLastnameError('');
+    }
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    if (event.target.value.length < 2) {
+      setEmailError('Email must bee at least 2 character.');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const passwordHandle = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value.length < 8) {
+      setPasswordError('Password must bee at least 8 character.');
+    } else {
+      setPasswordError('');
+    }
+  };
+
+  const confirmpasswordHandle = (event) => {
+    setConfirmpassword(event.target.value);
+    if (password !== event.target.value) {
+      setConfirmpasswordError('Passwords must match');
+    } else {
+      setConfirmpasswordError('');
+    }
+  };
 
   return (
     <div>
       <form>
         <div>
           <label>First Name:</label>
-          <input
-            type='text'
-            onChange={(event) => setUsername(event.target.value)}
-          />
+          <input type='text' onChange={handleFirstname} />
+          {usernameError ? <div>{usernameError}</div> : ''}
         </div>
 
         <div>
           <label>Last Name:</label>
-          <input
-            type='text'
-            onChange={(event) => setLastname(event.target.value)}
-          />
+          <input type='text' onChange={handleLastname} />
+          {lastnameError ? <div> {lastnameError} </div> : ''}
         </div>
 
         <div>
           <label>Email:</label>
-          <input
-            type='text'
-            onChange={(event) => setEmail(event.target.value)}
-          />
+          <input type='text' onChange={handleEmail} />
+          {emailError ? <div> {emailError} </div> : ''}
         </div>
 
         <div>
           <label>Password:</label>
-          <input
-            type='password'
-            onChange={(event) => setPassword(event.target.value)}
-          />
+          <input type='password' onChange={passwordHandle} />
+          {passwordError ? <div> {passwordError}</div> : ''}
         </div>
 
         <div>
           <label>Confirm Password:</label>
-          <input
-            type='password'
-            onChange={(event) => setConfirmpassword(event.target.value)}
-          />
+          <input type='password' onChange={confirmpasswordHandle} />
+          {confirmpasswordError ? <div>{confirmpasswordError}</div> : ''}
         </div>
       </form>
-
-      <div>
-        <h3>Your form data: </h3>
-        <p>First Name: {username}</p>
-        <p>Last Name: {lastname}</p>
-        <p>Email: {email}</p>
-        <p>Password: {password}</p>
-        <p>Confirm Password: {confirmpassword}</p>
-      </div>
     </div>
   );
 }
